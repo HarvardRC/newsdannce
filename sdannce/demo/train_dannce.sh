c535619401a9a38ccff020b554629fd1c0e40a10
tqxli <tianqing.li@duke.edu>
2022-12-22 13:24:28 -0500

    initial commit
    Former-commit-id: 3ccbcdb818044afc58de9928a93de933c3958cd0 [formerly 5076a7a16c065f3e6295e038297cc346f51eddd8] [formerly e78fdfd670ca82ef6e3a169f82f1c4efa8105c01 [formerly d0e239e2e15a0e3a1eca25aca7616095e3658dc0]] [formerly dc074a242432415a68df02f03a9fe9defcce7f2a [formerly 189ec2a4cfb355bc7722ec698188833f5506dfb4] [formerly 5999243e1ad3a0b0b9a6b9529681a89518f370d6 [formerly f57ca4a636b0aa7d02d439b3d0c04790181fd3ef [formerly ebe93d633f8f7c291e3f55242f3260f5fa2f4923] [formerly 4896ba578c900f249091ad715979e274a55127d4] [formerly d0e239e2e15a0e3a1eca25aca7616095e3658dc0 [formerly 189ec2a4cfb355bc7722ec698188833f5506dfb4 [formerly 189ec2a4cfb355bc7722ec698188833f5506dfb4 [formerly fa4d23e2ce9ecc3201899f64603bd6dbb348fcea]]]]]]]
    Former-commit-id: 576f9df37a5bd13eb315299697ab5ae96b87560f [formerly f4c3a3daed3d1082e819d84067ce7ff580ee218d] [formerly 7e1ac8f834531652aad77d2b81fb0196c62870f7 [formerly f57ca4a636b0aa7d02d439b3d0c04790181fd3ef [formerly ebe93d633f8f7c291e3f55242f3260f5fa2f4923] [formerly 4896ba578c900f249091ad715979e274a55127d4]]]
    Former-commit-id: eeae5a2e266c296460ed3507f97cfbcf3721c234 [formerly f57ca4a636b0aa7d02d439b3d0c04790181fd3ef [formerly ebe93d633f8f7c291e3f55242f3260f5fa2f4923]]
    Former-commit-id: c78bcad8ed45edc28f33d36ef49c041450907b81
    Former-commit-id: 817b2ebde7bf66e0edd9f4d718803eff9768cdd2 [formerly 631d846d1b3c2253b21080819355fa31d04c9262 [formerly 97d632f731dcd7719fbba1d8566790588a59d416] [formerly 1c63385651e9b7dfd9a1652822bb67a3e18e3581 [formerly 020ca23eb6f47897af3115155304abcd5c07ec9b]]]
    Former-commit-id: 2625a956206c9f772ac37501777e6ec12beb6031
    Former-commit-id: 2f874c15e0481cf8126dbdf6994f2b0b2fd4f008
    Former-commit-id: 8522f64cf44c4f853a0387c81107bb26160270cd

---
+++ sdannce/demo/train_dannce.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+cd ./2021_07_06_M3_M6
+echo "Training DANNCE..."
+
+dannce-train \
+	../../configs/dannce_rat_config.yaml \
+	--train-mode finetune \
+	--dannce-finetune-weights ../weights/DANNCE_comp_pretrained_single+r7m.pth \
+	--net-type compressed_dannce \
+	--use-npy True
+
+cd ..
+echo "DONE!"
