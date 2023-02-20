8e28df0e360573232dcecdf858977b614d227e58
Diego Aldarondo <diegoaldarondo@g.harvard.edu>
2023-02-20 12:51:13 -0500

    Edit prep script
    Former-commit-id: a69d1cb84a531e9997a1d1baf43a47c58c4c0785 [formerly 8b7d8a05711000e48a08ace348064f5193037784] [formerly d19ecec3dfd7d6d8997695f65ce0e4201e05aca8 [formerly abf58ece6cd420f1e7ff6e02bceb90def42ecc0a]] [formerly 443c148c87fda83119e2b4836c92b07beafc66e0 [formerly 00dc1287f25131b028bdab90a9f7cf699631e848] [formerly b2608da509ae8ca0128003acb1086ac897477224 [formerly f0e3546d7ee572ea20a6179a3a214bdc35a5379d]]]
    Former-commit-id: 59fc56105a475e074aaf8ecf8b539ea79c0ab99c [formerly 707336dfa2a3651bc46b03e857efcd97d2a7eb19] [formerly 19f172748093e230335753fc7b173e0115a5754f [formerly 6d9b2b17199a76a4240e5fe2deda580ba9524030]]
    Former-commit-id: 9fa96b0d8ff377efbc419ef13f803e3108799c5a [formerly 6938b8a7482e936f9a6c463e4594ffb3ec3b7004]
    Former-commit-id: fde15f4120ad6c607d5ec6aa9e8f5680774a5380

--- tests/prepare_test_data.sh
+++ tests/prepare_test_data.sh
@@ -2,10 +2,10 @@
 
 echo "Preparing test data..."
 
-# unzip demo_data.zip
-# mv demo_data/* .
-# rm demo_data.zip
-# rm demo_data
+unzip demo_data.zip
+mv demo_data/* .
+rm demo_data.zip
+rm demo_data
 
 cp test_files/com_io.yaml 2021_07_06_M3_M6/io.yaml
 cp test_files/com_io.yaml 2021_07_05_M4_M7/io.yaml
