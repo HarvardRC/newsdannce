c9d7ae2e1d821eb1b41467b9c219f911510eb838
tqxli <tianqing.li@duke.edu>
2023-02-20 19:35:00 -0500

    fix bug with social inference batch size
    Former-commit-id: 11775c4df83ff577b73c2abb4ad791c2b20be9cc [formerly 7b08f7b8d7de4af617f09557ad2afeb0557401a6] [formerly 3513c5168f95fcf44df4b7496142463010673196 [formerly 185dcff8b699d080baf1b9f1deda11f43ca55b27]] [formerly 5b3918205dd10cc1a35bb9552f21f6c95bcda4e7 [formerly 2ead071f169c7cf5941e77bd38c469d4bedd28fa] [formerly 601fef6c6ebf68ef4cfcd0c35a28a5895be8f4b6 [formerly 8101193bae587280186eb67cae1cb720518a5e1c [formerly 5eb43fd67c0ae349643706191f3b81def4f97944] [formerly 643045ad9dc7c6c8efa5fbec588c55c01b17b607] [formerly 185dcff8b699d080baf1b9f1deda11f43ca55b27 [formerly 2ead071f169c7cf5941e77bd38c469d4bedd28fa [formerly 2ead071f169c7cf5941e77bd38c469d4bedd28fa [formerly 3817b6b09b20916fe7b64aa85ce616979ab0d8bc]]]]]]]
    Former-commit-id: 5a720c1b2f431baeec5af71d4beadddcb07c297c [formerly 244a7579e2f506a213d016391820282528decee2] [formerly 0421daae7b6b07b8d2975af799a00383e8ba3dce [formerly 8101193bae587280186eb67cae1cb720518a5e1c [formerly 5eb43fd67c0ae349643706191f3b81def4f97944] [formerly 643045ad9dc7c6c8efa5fbec588c55c01b17b607]]]
    Former-commit-id: bb65ba116781d4b2b99cd42ccd684a413d31a04b [formerly 8101193bae587280186eb67cae1cb720518a5e1c [formerly 5eb43fd67c0ae349643706191f3b81def4f97944]]
    Former-commit-id: 0c769dd98e9168b6f502b28d9880e3c5b24ef910
    Former-commit-id: 35f1ab3f496638df35cc2aff577cadee9e31e467 [formerly f81eb1fb56ac11fdfc2e9f22df1d738c36c40d60 [formerly b539cb8d96ec181e6a30fe92d512246b06d30521] [formerly b602875cea1ea3d19a12a0a0aea269ad483de7ab [formerly c0826277402061e89b1d16e8af00d1d7296bd1dd]]]
    Former-commit-id: a345a3ceb4832d666d68ed942f057877b2d34a9c
    Former-commit-id: 8503d51c8a8316caa55eac45eff93d94c64427a9
    Former-commit-id: 74b1387518bccfb2c37a7b3f040cbbbccf8b4d18

--- demo/vis_sdannce.sh
+++ demo/vis_sdannce.sh
@@ -5,6 +5,6 @@ echo "Generating SDANNCE prediction video..."
 ROOT=2021_07_05_M4_M7
 PRED=SDANNCE/predict02
 
-python visualization/vis.py --root $ROOT --pred $PRED  --cameras 1
+python visualization/vis.py --root $ROOT --pred $PRED  --cameras 1 --n_frames 10
 
 echo "Saved to $ROOT/$PRED/vis"
