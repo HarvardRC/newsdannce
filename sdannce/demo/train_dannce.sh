264d684dedb33ca46a2afca58e30c5b287711f24
tqxli <tianqing.li@duke.edu>
2023-02-23 16:19:00 -0500

    update demo scripts with new cli
    Former-commit-id: bd4faa01311a30237b8423eb6651e310f6b51a16 [formerly c21cac2d766a8dddcac9cd3a454f7b36cde2c03b] [formerly b473778d5f3c377a713fac4162230e944a2ede1c [formerly ad95b45c1d2a37ac9229a04bfc70bce2e82eb57e]] [formerly bceb5f634c847f9dc179ea525ec557d8eb006bea [formerly d70c76545e125c6baf4aef592e46dc8c6b769914] [formerly efba16a293ddceea9a028edc82b798c5803ec8af [formerly fa470d1688f5dfbe87f51f73e9905b6ef20d9788]]]
    Former-commit-id: ff6af54430a37dd936e95b0cd99d5f38dca6308a [formerly f8eb4fd3cfee6be532061855b4396affa6d8cbbf] [formerly e6e28ba062e246073077447fd331e390ffac6b45 [formerly 0af222f2de921d754ebc5f769b1c1410ea92d8ba]]
    Former-commit-id: a78ad4c1ec12b08fc84bdcd1d3ff05bd952dee43 [formerly 068b062c972887ee8cde94f39ced30a214288043]
    Former-commit-id: 16f2f312748f933fa344b9b9a0578bc9141a9824

--- sdannce/demo/train_dannce.sh
+++ sdannce/demo/train_dannce.sh
@@ -3,7 +3,7 @@
 cd ./2021_07_06_M3_M6
 echo "Training DANNCE..."
 
-dannce-train \
+dannce train dannce \
 	../../configs/dannce_rat_config.yaml \
 	--train-mode finetune \
 	--dannce-finetune-weights ../weights/DANNCE_comp_pretrained_single+r7m.pth \
