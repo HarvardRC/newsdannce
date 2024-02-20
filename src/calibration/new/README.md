f563839e6441120e3bd98a59e34b192eab60bccf
caxon <christopher_axon@harvard.edu>
2024-02-19 23:40:45 -0500

    python calibration
    Former-commit-id: 50d9a72d55d7c2a35aded9d4c1189f844fac6e7d [formerly 484a2e71ff2c926e0e73b9b04eace51f2f2554d9] [formerly 720fe19bb56fb61b815999f6840cae4b88adaf23 [formerly 2806e40ffb292bee22e2c1e4609e641b178261b9]] [formerly cd8c07585ca8be8b00e82cfb324a2594201f36b9 [formerly 54d345c285723e3ce1322e4e778144fe268f44e7] [formerly 0bcf20a3c1390ae9b94bd0ded4e65895636d50aa [formerly e1d766a3b9434b183b6433f8fec576286066c5fd]]] [formerly 7524380ca5e67cb0af12b4b8c294f7f973383155 [formerly 3958aa29ca018dd2cecc7108b3e5f70e91883344] [formerly 1610e9c049c0595afbf4d395a789af1dc3d6f59c [formerly cfccd8b1071f536b5584b59d1ecedb7c0c742b1a]] [formerly e766d8431d9694dbc2d1bfd08c36d9288d1b298f [formerly 2251f4d3e7a31afeb8d662d4a8acff995427e98b] [formerly bbceea0dbfebf83fe4dffae4f9ee80cc7f7a8936 [formerly 9da6d207637f7771db899ab420c5c8bac389187a [formerly 20983c539e52d331690e5e1d2511ef5ef9efb577] [formerly 332535de48a395d6cba70985f3548f71fd18fb0e] [formerly e1d766a3b9434b183b6433f8fec576286066c5fd [formerly 2251f4d3e7a31afeb8d662d4a8acff995427e98b [formerly 2251f4d3e7a31afeb8d662d4a8acff995427e98b [formerly 9649e1e252515743629b3cc842f4a86b6f1fadc0]]]]]]]]
    Former-commit-id: 0f4f193ab3999ac74059b2d067520d28a5c104b7 [formerly 7302ff810c12a5aafe0f9932cf2924bddf5444df] [formerly 9fe2813ad7d561cc02f0f4233d60bb189ebe7a8b [formerly abfd181a0eeda2be0ccbe25cfdb03966057a0efd]] [formerly 056a4db01fd5d4ad04991a07453d8916ce4ad58a [formerly ac5f98fc7cf0dd27dbd552056e877911a524e0e0] [formerly de57bf8200d7bab3945d7838b535e1e8ce1bf065 [formerly 896536f3ec12c12525e27ab921ce99333e04388e]]]
    Former-commit-id: 752097c1a3281523b692f5fcebdaa1b48d98b13b [formerly 31147fd667566b063c873337482e8fca13c7bbc5] [formerly 01d6bdd39eb9b774cd8551f2ef8d382eb9060044 [formerly c2c77156ccb122960e14cea274b144fb518cc263]]
    Former-commit-id: 544d3a24b45b031355f018023e0cdaf3cd2734d1 [formerly 78b65134595ad7f1e4f6edf2ef0560e3167950dc]
    Former-commit-id: 9808802f7ab572af68419b7aa8cf69bca331b0d4
    Former-commit-id: 3a91c806e357589e0609b34155eabe0777575aca [formerly e590610ccc88911c26e11dd3aa7ad106f15ee289 [formerly 4fe90c19de0f5267df85e7ecf0ee1228b357364c] [formerly 752f321349e24558005d520043e81a74271259fb [formerly 927e784cdf64597cd5d05ae23744532b83a4d80d]]]
    Former-commit-id: 12e4d793a3a2a70c03db28f4e4e4d6ec788a6a6b
    Former-commit-id: 49e845cfdc62b6b4f572be6c164c9ce719e4b332
    Former-commit-id: e44ab292537837196d90fe63317e9b8b92150f57

---
+++ src/calibration/new/README.md
@@ -0,0 +1,15 @@
+# New Calibration (WIP)
+
+New calibration for `newsdannce` repo
+
+Idea: single canonical calibration pipeline
+
+Goals:
+- remove dependency on Matlab
+- robust, accurate calibration
+- easy to set up
+- well documented 
+
+How to run pytest:
+
+python -m pytest src/calibration/new
--- src/calibration/new/extrinsics.py
+++ src/calibration/new/extrinsics.py
@@ -1,119 +1,44 @@
-import cv2
+from dataclasses import dataclass
 import numpy as np
-from scipy.io import loadmat, savemat
+from src.calibration.new.intrinsics import IntrinsicsParams
+from src.calibration.new.utils import get_first_frame_video
+import cv2
 
-# termination criteria for subpixel corner detection -- constant for now
-CORNER_TERMINATION_CRITERIA = (
-    cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER,
-    30,
-    0.001,
-)
 
-## IMPORTANT NOTE: findChessboardCorners requires white space (like a 1 square thick, white border, wider is better)
-# around board to make detection more robust! This might be a reason why detection was failing for some images.
+@dataclass(frozen=True)
+class ExtrinsicsParams:
+    """Camera extrinsics parameters:
+    - rotation_matrix: 3x3 rotation transformation matrix - np.ndarray, Size=(3,3). AKA "r"
+    - translation_vector: [tx, ty, tz] - np.ndarray, Size=(3,). AKA "t"
+    """
 
-def compute_extrinsics(
-    undistorted_images_path,
-    output_matrix_path,
-    checkerboard_shape,
-    checkerboard_size_mm,
+    rotation_matrix: np.ndarray
+    translation_vector: np.ndarray
+
+
+def calibrate_extrinsics(
+    video_path: str,
+    rows: int,
+    cols: int,
+    object_points: np.ndarray,  # 3d points the calibration object. E.g. shape: (rows x cols, 3)
+    intrinsics: IntrinsicsParams,
+    image_width: int,
+    image_height: int,
 ):
-    if len(checkerboard_shape) != 2:
-        raise Exception("Checkerboard shape must be a 2-tuple E.g. (6, 9)")
+    # load distorted image from video frame
 
-    # number of internal points in the checkerboard calibration pattern
-    # IMPORTANT NOTE: this is the # of internal vertices - which is 1 less than the # of squares in ea. direction
-    CHECKERBOARD_VERTICIES_ROW = checkerboard_shape[0]
-    CHECKERBOARD_VERTICIES_COL = checkerboard_shape[1]
+    distorted_img = get_first_frame_video(video_path=video_path)
 
-    print("CHECKERBOARD SHAPE: ", checkerboard_shape, " ROWS: ", CHECKERBOARD_VERTICIES_ROW, " COLS: ", CHECKERBOARD_VERTICIES_COL, " SIZE: ", checkerboard_size_mm)
-
-    print(f"Reading undistorated images from: {undistorted_images_path}")
-
-    try:
-        m = loadmat(undistorted_images_path)
-        undistorted_images = m["undistortedIms"][:]
-    except FileNotFoundError:
-        raise Exception(
-            f"Failed to load undistorted images from {undistorted_images_path}. File not found."
-        )
-    except KeyError:
-        raise Exception(
-            "Undistorted images file expected to contain field: 'undistortedIms' shape e.g. (1200, 1920, 3, #cameras))"
-        )
-
-    # checkerboard vertices without units
-    # e.g. (0,0,0), (1,0,0), (2,0,0) ... (0,1,0) ... (3,4,0) ...
-    object_points_nounit = np.zeros(
-        (CHECKERBOARD_VERTICIES_COL * CHECKERBOARD_VERTICIES_ROW, 3), np.float32
+    # convert from r,t distort to single dist matrix (k1, k2, p1, p2)
+    dist_coeffs = np.hstack((intrinsics.r_distort, intrinsics.t_distort))
+    undistorted_img = cv2.undistort(
+        src=distorted_img, cameraMatrix=intrinsics.camera_matrix, distCoeffs=dist_coeffs
     )
-    object_points_nounit[:, 0:2] = (
-        np.mgrid[0:CHECKERBOARD_VERTICIES_COL, 0:CHECKERBOARD_VERTICIES_ROW].T.reshape(
-            -1, 2
-        )
-    ) 
-    object_points_mm = object_points_nounit * checkerboard_size_mm
 
-    # Arrays to store object points and image points from all the images.
-    all_object_points = []  # 3d coordinates of checkerboard vertex in real world (mm)
-    image_points = []  # 2d coordinates of checkerboard vertex in image plane (mm)
+    # undistort frame using intrinsics
 
-    num_cameras = undistorted_images.shape[3]
+    #
 
-    failed_list = []
-
-    for camera_idx in range(num_cameras):
-        camera_name = f"Camera{camera_idx+1}"
-
-        # assume image is stored in RGB format (e.g. from Matlab)
-        img_rgb = cv2.UMat(undistorted_images[:, :, :, camera_idx].squeeze())
-        img_bgr = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2BGR)
-        img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
-
-        # Find the chess board corners
-        success, approx_corners = cv2.findChessboardCorners(
-            img_bgr, checkerboard_shape, None
-        )
-
-        if success:
-            print(f"Successfully found checkerboard in camera: {camera_name}")
-            all_object_points.append(object_points_mm)
-
-            # find subpixel coordinates
-            corner_subpix_coords = cv2.cornerSubPix(
-                img_gray,
-                approx_corners,
-                (checkerboard_size_mm // 2, checkerboard_size_mm // 2),
-                (-1, -1),
-                CORNER_TERMINATION_CRITERIA,
-            )
-
-            image_points.append(cv2.UMat.get(corner_subpix_coords))
-
-            # Draw and display the corners
-            img_with_pattern = cv2.drawChessboardCorners(
-                img_bgr, checkerboard_shape, corner_subpix_coords, success
-            )
-
-            cv2.imshow(f"{camera_name} with pattern", img_with_pattern)
-            cv2.waitKey(1000)
-        else:  
-            failed_list.append(camera_name)
-
-    if len(failed_list) > 0:
-        print(f"Failed to find checkerboard in {failed_list}")
-        raise Exception(
-            f"Board not found in at least one image.\nFailed for cameras: {failed_list}"
-        )
-
-    cv2.destroyAllWindows()
-    mdict = {"objPoints": all_object_points, "imgPoints": image_points}
-    print(f"Saving extrinsics to: {output_matrix_path}")
-    savemat(output_matrix_path, mdict)
-
-compute_extrinsics(
-    undistorted_images_path="/Users/caxon/olveczky/dannce_data/test_chris_hannah/calPyScratch.mat",
-    output_matrix_path="/Users/caxon/olveczky/dannce_data/test_chris_hannah/test_output.mat",
-    checkerboard_shape=(6, 9),
-    checkerboard_size_mm=23,
-)
+    return ExtrinsicsParams(
+        rotation_matrix=np.zeros((3, 3)), translation_vector=np.zeros((3,))
+    )
