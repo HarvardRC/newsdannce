ab3af9aedbfea36e8d375c53c90b7cb50de0bcd7
caxon <christopher_axon@harvard.edu>
2024-03-25 14:39:58 -0400

    validate extrinsics & intrinsics
    Former-commit-id: 1da5180e44ab4c50aba8332dccba895d037ebe54 [formerly ba127c00ead8c3f8d9d618b80aa2d1e05bf99acd] [formerly eebfdfd234fa1d4f5bf1766843c5be9a7177e45e [formerly ecacec0e120a58bf9ccc35d51fa4c6abfde226cc]] [formerly e0a2f99d4577812e2c20b3290f4faeeec741b6c7 [formerly 0d4a25cd4f4d503ac159fa6cadf9044e4a89fa82] [formerly 5637a2c9a4a2e91e880c9c1b29d36d41949442b3 [formerly a3b54b75f99afb73caf142253e813a1213f10761]]] [formerly b234be5cb04d19af6253022aa72a6fa79573a77e [formerly 27f4632661434e93075848b44bf0684d14f18c84] [formerly 26cbe44c7720328bbe6be9384a134fc13a0f64af [formerly 579737227810a116d5ddc62a45034336d3454a7a]] [formerly 9fba402a3e99c5a0d341ff7ad7a08311ac1ae390 [formerly 61ea7372dcdf549c0cecc9c22276f5addfe5ab01] [formerly e48220221f86855da2fb29025a2409a7bd8b6101 [formerly ec93662e11aab607bb5c1bbc6546baff77644b95 [formerly ff250dbc800bfc7f041fce46a096707df0133479] [formerly b15df5083a6c8c6ebeba89c44cd573992cc4af0c] [formerly a3b54b75f99afb73caf142253e813a1213f10761 [formerly 61ea7372dcdf549c0cecc9c22276f5addfe5ab01 [formerly 61ea7372dcdf549c0cecc9c22276f5addfe5ab01 [formerly fe617e8814e668b43a905d6c82b14c8583629b62]]]]]]]]
    Former-commit-id: a41bd3f4ded59de6fac0c01eb6c1841d64dc8a91 [formerly 82e237e5ab526d6761ecb924fc683f0aeaaffbfc] [formerly e30250576aba49f76514f41cd9a8e2f9c4d2454c [formerly aa7b8177166c432fb1d807a970bdfabca564d5b9]] [formerly 6ca7dbb2a253ec20ce96a16cb94fc3dd3f9b18f6 [formerly e4dbbae214234d84b911d5e03129be18870977fb] [formerly 364887b590e5b01c1291fb99f753dcd4fc92a32f [formerly 5efc912c5b0d5fc333834ae87904de3ac7715f9d]]]
    Former-commit-id: 3bd90e2d64564fde05209939a8aed5ceb47e8477 [formerly 01ce690da3378e5e82232c1bb56473e0f76292f0] [formerly 70609dc953aa306b658f862412f97e5bf75bb455 [formerly f981494151477642f261b8d236e887ec158d7833]]
    Former-commit-id: 3f11e51b50d4ebba41d4b11ead9e5d1b8a9f4701 [formerly 0111c4e22895cb87953f7f2f7d32fa3b1531eb2f]
    Former-commit-id: d93b56c7eabbd09ee3b515d441529d7ab571cf6d
    Former-commit-id: 16d92b4d60ddfba0b21a57e0d0147c47dcf20f04 [formerly 8a950669de104b0b9b67c7ceeec0fcf8e48164c1 [formerly de73775eb318e5b55cf3895fa8cedf59472fa72f] [formerly 492c0ec034961cab6c8e1da4609b5821f5c36a8e [formerly 48c7bc2a6d15becd9ccf7595973659fad94d92e2]]]
    Former-commit-id: 96a7d49038e05184880a6d452691d5e1c8fa62c9
    Former-commit-id: 10a5669268b828b9c9c80c21d5b1e3850ea74448
    Former-commit-id: 8ba92a1ae1666618861496676032f26c85e895d6

--- src/calibration/new/intrinsics.py
+++ src/calibration/new/intrinsics.py
@@ -4,11 +4,21 @@ import cv2
 import time
 import json
 from typing import Optional
+from scipy.io import loadmat, savemat
+import pickle
+from .math_utils import calculate_rpe
+from pathlib import Path
+
+import matplotlib
+from matplotlib import pyplot as plt
+
+import sys
 
 show_img = False
+write_one = True
 
 
-@dataclass(frozen=True, slots=True, kw_only=True)
+@dataclass(frozen=True, slots=True, kw_only=True, eq=False)
 class IntrinsicsParams:
     """Camera intrinsics parameters:
     - camera_matrix: 3x3 intrinsics matrix - np.ndarray, Size=(3,3). AKA "K"
@@ -17,28 +27,56 @@ class IntrinsicsParams:
     """
 
     camera_matrix: np.ndarray
-    dist: np.ndarray
+    dist: np.ndarray  # format: [k1 k2 (radial) p1 p2 (tangential) [k3 (radial)] ]. k3 ignored.
 
     @property
-    def r_distort(self) -> np.ndarray:
+    def r_distort(self) -> np.ndarray:  # k1 k2 [k3]
         return self.dist[0:2]
 
     @property
-    def t_distort(self) -> np.ndarray:
+    def t_distort(self) -> np.ndarray:  # p1 p2
         return self.dist[2:4]
 
+    @staticmethod
+    def load_from_mat_file(
+        path: str, cvt_matlab_to_cv2: bool = False
+    ) -> "IntrinsicsParams":
+        """
+        cvt_matlab_to_cv2: if this is true, convert from matlab intrinsics matrix to cv2 intrinsics matrix (add 1)
+        """
+        mat_file = loadmat(path)
+        r_distort = np.array(mat_file["RDistort"]).squeeze()
+        t_distort = np.array(mat_file["TDistort"]).squeeze()
+        dist = np.array([r_distort[0], r_distort[1], t_distort[0], t_distort[1]])
+        k = mat_file["K"]
+
+        if cvt_matlab_to_cv2:
+            k[0, 2] -= 1
+            k[1, 2] -= 1
+
+        return IntrinsicsParams(camera_matrix=k, dist=dist)
+
     # reprojection_error: Optional[np.ndarray] = None
+
+    def __eq__(self, other: "IntrinsicsParams") -> bool:
+        return np.array_equal(self.dist, other.dist) and np.array_equal(
+            self.camera_matrix, other.camera_matrix
+        )
 
     def __post_init__(self):
         skew = self.camera_matrix[0, 1]
         if skew != 0:
             raise NotImplementedError("IntrinsicsParams does not support nonzero skew")
+        if len(self.dist) >= 5 and self.dist[4] != 0:
+            raise NotImplementedError(
+                "IntrinsicsParams does not support nonzero k3 in dist: [k1 k2 p1 p2 k3]"
+            )
 
     def __repr__(self) -> str:
         dist = np.hstack((self.r_distort, self.t_distort))
         return f"camera_matrix={json.dumps(self.camera_matrix.tolist())}\ndist={json.dumps(dist.tolist())}\nr_distort={self.r_distort}, t_distort={self.t_distort}"
 
-    def to_matlab(self, image_size=(1200, 1920)) -> "IntrinsicsParamsMatlab":
+    def to_matlab(self) -> "IntrinsicsParamsMatlab":
         f_x = self.camera_matrix[0, 0]
         f_y = self.camera_matrix[1, 1]
         c_x = self.camera_matrix[0, 2] + 1  # convert to matlab (1,1) origin
@@ -47,7 +85,7 @@ class IntrinsicsParams:
         ret = IntrinsicsParamsMatlab(
             radial_distortion=tuple(self.t_distort),
             tangential_distortion=tuple(self.r_distort),
-            image_size=image_size,
+            # image_size=image_size,
             focal_length=(f_x, f_y),
             principal_point=(c_x, c_y),
         )
@@ -60,7 +98,7 @@ class IntrinsicsParamsMatlab:
 
     focal_length: tuple[float, float]  # [fx, fy] in px
     principal_point: tuple[float, float]  # [cx, cy] in px
-    image_size: tuple[int, int]  # [mrows, ncols] i.e. [height width]
+    # image_size: tuple[int, int]  # [mrows, ncols] i.e. [height width]
     radial_distortion: tuple[float, float] = (0.0, 0.0)  # [k1, k1] (fix k3 to 0)
     tangential_distortion: tuple[float, float] = (0.0, 0.0)  # [p1, p2]
 
@@ -91,6 +129,31 @@ class IntrinsicsParamsMatlab:
         )
         return ret
 
+    # @staticmethod
+    # def load_from_mat(path: str):
+    #     mat_file = loadmat(path)
+    #     r_distort = np.array(mat_file["RDistort"]).squeeze()
+    #     t_distort = np.array(mat_file["TDistort"]).squeeze()
+    #     k = mat_file["K"]
+    #     # check if camera matrix is upper-triangular -- it should be lower-triangular
+    #     if np.any(np.triu(v=k, k=-1) > 0):
+    #         raise Exception(
+    #             f"Camera matrix is upper triangular, should be lower triangular.\nK={k}"
+    #         )
+    #     f_x = k[0, 0]
+    #     f_y = k[1, 1]
+    #     c_x = k[0, 2]
+    #     c_y = k[1, 2]
+    #     skew = k[0, 1]
+    #     if skew >= 0:
+    #         raise Exception(f"Does not support non-zero skew.\nskew={skew}")
+
+    #     ret = IntrinsicsParamsMatlab(
+    #         focal_length={f_x, f_y},
+    #         principal_point={c_x, c_y},
+    #     )
+    #     return ret
+
 
 def load_images(image_paths, image_width, image_height) -> np.ndarray:
     n_images = len(image_paths)
@@ -111,6 +174,7 @@ def calibrate_intrinsics(
     object_points: np.ndarray,  # 3d points the calibration object. E.g. shape: (rows x cols, 3)
     image_width: int,  # image width in px
     image_height: int,  # image height in px
+    camera_idx: int = None,  # optionally provide the camera idx (only useful for logging/debugging)
 ) -> IntrinsicsParams:
     n_images = len(image_paths)
     raw_images = load_images(
@@ -119,6 +183,7 @@ def calibrate_intrinsics(
 
     objpoints = []
     imgpoints = []
+    failed_imgs = []
 
     start = time.perf_counter()
 
@@ -138,21 +203,56 @@ def calibrate_intrinsics(
                 cv2.drawChessboardCorners(this_img, (cols, rows), corner_coords, True)
         else:
             print("Failure: Image #", img_idx)
+            failed_imgs.append(img_idx)
 
     end = time.perf_counter()
 
     print(f"Found all corners in {(end-start)*1000:.2f} ms [{n_images} images]")
 
     # note: we ignore r_vecs & t_vecs because we don't care about location of calibration target in each frame
-    reproject_err, camera_matrix, raw_dist, _r_vecs, _t_vecs = cv2.calibrateCamera(
+    reproject_err, camera_matrix, raw_dist, r_vecs, t_vecs = cv2.calibrateCamera(
         objpoints, imgpoints, gray.shape[::-1], None, None, flags=cv2.CALIB_FIX_K3
     )
+
     print(reproject_err)
 
     dist = raw_dist.squeeze()
 
-    return IntrinsicsParams(
+    ret = IntrinsicsParams(
         camera_matrix=camera_matrix,
         dist=dist,
         # rpe=reproject_err,
     )
+
+    # TODO: REMOVE
+    # plot RPE for testing
+
+    rpes = []
+    n_images_success = len(imgpoints)
+    for i in range(n_images_success):
+        ipts = imgpoints[i].squeeze()
+        re_ipts = cv2.projectPoints(
+            objpoints[i],
+            r_vecs[i],
+            t_vecs[i],
+            camera_matrix,
+            dist,
+        )
+        re_ipts = re_ipts[0].squeeze()
+        rpe = calculate_rpe(ipts, re_ipts)
+        rpes.append(rpe)
+
+    print("INTRINSICS MEAN RPE:", np.mean(rpes))
+    print("INTRINSICS MAX  RPE:", np.max(rpes))
+    print("INTRINSICS MIN  RPE:", np.min(rpes))
+
+    # use non-interactive MPL backend
+    matplotlib.use("agg")
+    plt.figure()
+    plt.bar(x=[i for i in range(len(rpes))], height=rpes)
+    plt.title(f"Camera {camera_idx+1} Intrinsics RPE")
+    Path("./out").mkdir(parents=True, exist_ok=True)
+    plt.savefig(f"./out/rpe-cam{camera_idx+1}.png")
+
+    # TODO: END REMOVE
+    return ret
---
+++ src/calibration/new/vs_matlab.py
@@ -0,0 +1,195 @@
+import cv2
+import os
+
+import argparse
+
+import pathlib
+
+from .extrinsics import ExtrinsicsParams
+from .intrinsics import IntrinsicsParams
+from .video_utils import get_first_frame_video, get_chessboard_coordinates, load_image
+
+from .math_utils import calculate_rpe
+
+
+def test_matlab(
+    hires_file,
+    img,
+    output_dir,
+    rows=6,
+    cols=9,
+    square_size_mm=23,
+):
+    intrinsics = IntrinsicsParams.load_from_mat_file(hires_file, cvt_matlab_to_cv2=True)
+    extrinsics = ExtrinsicsParams.load_from_mat_file(hires_file)
+
+    object_points = get_chessboard_coordinates(
+        chessboard_rows=rows,
+        chessboard_cols=cols,
+        square_size_mm=square_size_mm,
+    )
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    success, image_points = cv2.findChessboardCorners(gray, (cols, rows), None)
+
+    if success is False:
+        raise Exception(
+            "Chessboard corners not found - unable to test extrisnics params [matlab]"
+        )
+
+    ipts = image_points.squeeze()
+    re_ipts_raw = cv2.projectPoints(
+        objectPoints=object_points,
+        rvec=extrinsics.r_vec,
+        tvec=extrinsics.translation_vector,
+        cameraMatrix=intrinsics.camera_matrix,
+        distCoeffs=intrinsics.dist,
+    )
+    re_ipts = re_ipts_raw[0].squeeze()
+    rpe = calculate_rpe(ipts, re_ipts)
+
+    draw_img = img.copy()
+
+    cv2.drawChessboardCorners(
+        image=draw_img,
+        patternSize=(cols, rows),
+        corners=re_ipts,
+        patternWasFound=False,
+    )
+
+    out_path = os.path.join(output_dir, "matlab-reprojections.png")
+    cv2.imwrite(img=draw_img, filename=out_path)
+
+    print("MATLAB RPE is ", rpe)
+
+    return {"rpe": rpe, "re_imgpoints": re_ipts}
+
+
+def test_cv2(hires_file, img, output_dir, rows=6, cols=9, square_size_mm=23):
+    intrinsics = IntrinsicsParams.load_from_mat_file(
+        hires_file, cvt_matlab_to_cv2=False
+    )
+    extrinsics = ExtrinsicsParams.load_from_mat_file(hires_file)
+
+    object_points = get_chessboard_coordinates(
+        chessboard_rows=rows,
+        chessboard_cols=cols,
+        square_size_mm=square_size_mm,
+    )
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    success, image_points = cv2.findChessboardCorners(gray, (cols, rows), None)
+
+    if success is False:
+        raise Exception(
+            "Chessboard corners not found - unable to test extrisnics params [cv2]"
+        )
+
+    ipts = image_points.squeeze()
+    re_ipts_raw = cv2.projectPoints(
+        objectPoints=object_points,
+        rvec=extrinsics.r_vec,
+        tvec=extrinsics.translation_vector,
+        cameraMatrix=intrinsics.camera_matrix,
+        distCoeffs=intrinsics.dist,
+    )
+    re_ipts = re_ipts_raw[0].squeeze()
+    rpe = calculate_rpe(ipts, re_ipts)
+
+    draw_img = img.copy()
+
+    cv2.drawChessboardCorners(
+        image=draw_img,
+        patternSize=(cols, rows),
+        corners=re_ipts,
+        patternWasFound=False,
+    )
+
+    cv2.drawChessboardCorners(
+        image=draw_img,
+        patternSize=(cols, rows),
+        corners=image_points.squeeze(),
+        patternWasFound=True,
+    )
+
+    out_path = os.path.join(output_dir, "cv2-reprojections.png")
+    cv2.imwrite(img=draw_img, filename=out_path)
+
+    print("CV2 RPE is ", rpe)
+
+    return {"rpe": rpe, "re_imgpoints": re_ipts}
+
+
+def do_compare(matlab_hires_file, cv2_hires_file, img, output_dir="./out"):
+    test_matlab(hires_file=matlab_hires_file, img=img, output_dir=output_dir)
+    test_cv2(hires_file=cv2_hires_file, img=img, output_dir=output_dir)
+    print("DONE!")
+
+
+def parse_and_compare():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-o",
+        "--output-dir",
+        required=False,
+        default="./out",
+        help="Output directory to create hires_cam#_params.mat files. If not provided, will print calibration params to the console.",
+    )
+    parser.add_argument(
+        "-m",
+        "--matlab-hires-file",
+        required=True,
+        help="Path to hires_camX_param.mat file from matlab calibration to compare",
+    )
+    parser.add_argument(
+        "-c",
+        "--cv2-hires-file",
+        required=True,
+        help="Path to hires_camX_param.mat file from cv2 calibration to compare",
+    )
+    parser.add_argument(
+        "-v",
+        "--video-input",
+        required=False,
+        default=None,
+        help="Path video file containing a checkerboard to test (EITHER THIS OR --image-input must be specified)",
+    )
+    parser.add_argument(
+        "-i",
+        "--image-input",
+        required=False,
+        default=None,
+        help="Path image file containing a checkerboard to test (EITHER THIS OR --video-input must be specified)",
+    )
+
+    args = parser.parse_args()
+    output_dir = args.output_dir
+    matlab_hires_file = args.matlab_hires_file
+    cv2_hires_file = args.cv2_hires_file
+    video_input = args.video_input
+    image_input = args.image_input
+    if (video_input is None and image_input is None) or (
+        video_input is not None and image_input is not None
+    ):
+        raise Exception(
+            "Must specifiy either --video-input OR --image-input, but NOT BOTH"
+        )
+
+    if video_input:
+        img = get_first_frame_video(video_path=video_input)
+    else:
+        img = load_image(image_path=image_input)
+
+    print("Making output directory if does not exist: ", output_dir)
+    pathlib.Path(output_dir).mkdir(exist_ok=True, parents=True)
+
+    do_compare(
+        matlab_hires_file=matlab_hires_file,
+        cv2_hires_file=cv2_hires_file,
+        img=img,
+        output_dir=output_dir,
+    )
+
+
+if __name__ == "__main__":
+    parse_and_compare()
