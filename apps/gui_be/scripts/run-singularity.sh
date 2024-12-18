2797fc8f8adc8ad111c4c0ff45ce7b1801d5331d
Chris <caxon26@gmail.com>
2024-12-18 16:41:30 -0500

    update run-singularity

--- apps/gui_be/scripts/run-singularity.sh
+++ apps/gui_be/scripts/run-singularity.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 FASTAPI_PORT=7911
-RABBITMQ_NODE_PORT=7912
+RABBITMQ_PORT=7912
 
-BASE_VOLUME=~/dannce-data-singularity
+BASE_MOUNT=~/dannce-data-singularity
 
-mkdir -m777 -p $BASE_VOLUME
+mkdir -m777 -p $BASE_MOUNT
 
 ENV_TEMPFILE=$(mktemp)
 cat <<EOT >> $ENV_TEMPFILE
@@ -20,18 +20,8 @@ echo "Created env file at $ENV_TEMPFILE"
 cat $ENV_TEMPFILE
 
 #### start shell in the container
-# singularity exec dannce-gui.sif /usr/local/bin/_entrypoint.sh /bin/bash
-# singularity run dannce-gui.sif
 
 singularity run \
     --bind $BASE_MOUNT:/mnt-data \
     --env-file $ENV_FILE \
-    dannce-gui.sif
-
-# singularity exec \
-#     --bind $ENV_FILE_PATH:/mnt-data/.env \
-#     --bind $INSTANCE_DIR:/mnt-data/instance \
-#     --bind $TEMP_DIR:/tmp \
-#     --bind $LOGS_DIR:/mnt-data/logs \
-#     --bind $RABBITMQ_MNESIA_DIR:/mnt-data/rabbitmq_mnesia \
-#     dannce-gui.sif /usr/local/bin/_entrypoint.sh /bin/bash
+    ./dannce-gui.sif
