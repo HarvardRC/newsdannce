3f8fa32587ffa68eaf9c6d43daa588291c521455
caxon <christopher_axon@harvard.edu>
2025-01-16 18:15:13 -0500

    fix docker shell script

--- apps/gui_be/scripts/shell-docker.sh
+++ apps/gui_be/scripts/shell-docker.sh
@@ -2,29 +2,40 @@
 
 # these ports must match the ports in dev.env file
 FASTAPI_PORT=7801
-RABBITMQ_NODE_PORT=7802
+RABBITMQ_PORT=7802
+FLOWER_PORT=7903
+BASE_MOUNT=~/dannce-data
 
-INSTANCE_DIR=/tmp/dannce_gui_data 
-ENV_FILE_PATH=./dev.env
-TEMP_DIR=/tmp/dannce_gui
 DATA_FOLDER=/Users/caxon/olveczky/dannce_data
 
-# docker run \
-#   -it dannce-gui \
-#   --entrypoint "/app/scripts/start_from_container.sh" \
-#   /bi/bash
-echo 'source /mnt-data/.env after container starts'
+# make a env file at a temporary location
+ENV_TEMPFILE=$(mktemp)
+cat <<EOT >> $ENV_TEMPFILE
+FASTAPI_PORT=${FASTAPI_PORT}
+RABBITMQ_PORT=${RABBITMQ_PORT}
+FLOWER_PORT=${FLOWER_PORT}
+SERVER_BASE_URL=http://localhost:${FASTAPI_PORT}
+API_BASE_URL=http://localhost:${FASTAPI_PORT}/v1
+REACT_APP_BASE_URL=http://localhost:${FASTAPI_PORT}/app/index.html
+SDANNCE_SINGULARITY_IMG_PATH=NOT_SET_ON_LOCALHOST
+EOT
+
+echo "Created env file at $ENV_TEMPFILE"
+cat $ENV_TEMPFILE
+
 
 docker run \
     --rm \
     -it \
     -p ${FASTAPI_PORT}:${FASTAPI_PORT} \
-    -v $INSTANCE_DIR:/mnt-data/instance \
-    -v $ENV_FILE_PATH:/mnt-data/.env \
-    -v $TEMP_DIR:/tmp \
+    -p ${FLOWER_PORT}:${FLOWER_PORT} \
+    -v $BASE_MOUNT:/mnt-data \
+    -v ./scripts:/app/scripts \
     -v ./src:/app/src \
     -v ./resources:/app/resources \
     -v $DATA_FOLDER:$DATA_FOLDER \
-    --entrypoint "/usr/local/bin/_entrypoint.sh" \
+    --env-file ${ENV_TEMPFILE} \
+    --read-only \
+    --entrypoint /usr/local/bin/_entrypoint.sh \
     dannce-gui \
     /bin/bash
