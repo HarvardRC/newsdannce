3512f665cfdccd7dc13cc5288f9e7b9122b46614
caxon <christopher_axon@harvard.edu>
2025-01-21 17:21:21 -0500

    fixed requirement for secrets.json

--- apps/gui_fe/vite.config.ts
+++ apps/gui_fe/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import secrets from './src/secrets.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -25,64 +24,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    proxy: {
-      '/v1': {
-        target: secrets.base_api_url,
-        rewrite: (path) => path.replace(/^\/v1/, 'v1'),
-        changeOrigin: false,
-        secure: false,
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
-            proxyReq.appendHeader(
-              'Authorization',
-              `Basic ${secrets.basic_auth_token}`
-            );
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log(
-              'Received Response from the Target:',
-              proxyRes.statusCode,
-              req.url
-            );
-          });
-        },
-      },
-      '/static': {
-        target: secrets.base_api_url,
-        rewrite: (path) => path.replace(/^\/v1/, 'static'),
-        changeOrigin: false,
-        secure: false,
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('[static] proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log(
-              '[static] Sending Request to the Target:',
-              req.method,
-              req.url
-            );
-            proxyReq.appendHeader(
-              'Authorization',
-              `Basic ${secrets.basic_auth_token}`
-            );
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log(
-              '[static] Received Response from the Target:',
-              proxyRes.statusCode,
-              req.url
-            );
-          });
-        },
-      },
     },
   },
 });
